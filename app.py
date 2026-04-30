ad84325a9e1faca59bb922b2f2a8d736069b35c6
Ayesha Muhammad Farooqi <ayeshamfarooqi@gmail.com>
2026-04-30 11:31:40 +0500

    second commits
    changed the model to logistics
    corrected name to match in expected columns

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model
-model = joblib.load('KNN_Heart.pkl')
+model = joblib.load('LGR_Heart.pkl')
 scaler = joblib.load('scaler.pkl')
 expected_cols = joblib.load('columns.pkl')
 
@@ -11,42 +11,49 @@ expected_cols = joblib.load('columns.pkl')
 st.title('Heart Disease Prediction by Ayesha')
 st.markdown('## Input Parameters')
 
-age = st.slider('Age', 29, 77, 54)
+age = st.slider('Age', 0, 100, 54)
 sex = st.selectbox('SEX',['M', 'F'])
 chest_pain_type = st.selectbox('Chest Pain Type', ['TA', 'ATA', 'NAP', 'ASY'])
-resting_blood_pressure = st.slider('Resting Blood Pressure', 94, 200, 130)
-serum_cholesterol = st.slider('Serum Cholesterol', 126, 600, 200)
+resting_blood_pressure = st.number_input('Resting Blood Pressure', 94, 200, 130)
+serum_cholesterol = st.number_input('Serum Cholesterol', 126, 600, 200)
 fasting_blood_sugar = st.selectbox('Fasting Blood Sugar > 120 mg/dl', ['0', '1'])
 rest_ecg = st.selectbox('Resting ECG', ['Normal', 'ST-T wave abnormality', 'Left ventricular hypertrophy'])
 max_heart_rate = st.slider('Max Heart Rate', 71, 202, 150)
-exercise_induced_angina = st.selectbox('Exercise Induced Angina', ['0', '1'])
+exercise_induced_angina = st.selectbox('Exercise Induced Angina', ['Y', 'N'])
 oldpeak = st.slider('Oldpeak', 0.0, 6.2, 1.0)
 slope = st.selectbox('Slope', ['Upsloping', 'Flat', 'Downsloping'])
 
 if st.button('Predict'):
     raw_input = {
-        'age': age,
-        'sex': 1 if sex == 'M' else 0,
-        'chest_pain_type': chest_pain_type,
-        'resting_blood_pressure': resting_blood_pressure,
-        'serum_cholesterol': serum_cholesterol,
-        'fasting_blood_sugar': int(fasting_blood_sugar),
-        'rest_ecg': rest_ecg,
-        'max_heart_rate': max_heart_rate,
-        'exercise_induced_angina': int(exercise_induced_angina),
-        'oldpeak': oldpeak,
-        'slope': slope
-    }
+    'Age': age,
+    'RestingBP': resting_blood_pressure,
+    'Cholesterol': serum_cholesterol,
+    'MaxHR': max_heart_rate,
+    'Oldpeak': oldpeak,
+    'FastingBS': fasting_blood_sugar,
+    'ExerciseInducedAngina': exercise_induced_angina,
+    'Sex_': sex,
+    'ChestPainType_': chest_pain_type,
+    'RestECG_': rest_ecg,
+    'ST_Slop_': slope
+}
+
 
     input_df = pd.DataFrame([raw_input])
-    for col in expected_cols:
-        if col not in input_df.columns:
-            input_df[col] = 0
+    
+    # One-hot encode categorical features
+    input_df = pd.get_dummies(input_df)
 
-    input_df = input_df[expected_cols]
+    # Align with training columns
+    input_df = input_df.reindex(columns=expected_cols, fill_value=0)
+
     input_scaled = scaler.transform(input_df)
-    prediction = model.predict(input_scaled)[0]
 
+    prediction = model.predict(input_scaled)[0]
+    proba = model.predict_proba(input_scaled)[0]
+    st.write("Probability: ", proba)
+
+    # prediction = 1 if proba[1] >= 0.3 else 0
     if prediction == 1:
         st.error('The model predicts that you have heart disease.')
     else:
